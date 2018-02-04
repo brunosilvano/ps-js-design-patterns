ddde60daea59ddf4fc7ead2c7b7a0b86ae59a695
Bruno Silvano Costa <bruno_705@hotmail.com>
2018-02-04 01:02:52 -0200

    Add js Class example

---
+++ Creational/Constructor/class.js
@@ -0,0 +1,19 @@
+'use strict'
+
+class Task {
+  constructor(name) {
+    this.name = name;
+    this.completed = false;
+  };
+
+  complete() {
+    console.log('completing Task: ' + this.name);
+    this.completed = true;
+  };
+
+  save() {
+    console.log('saving Task: ' + this.name);
+  };
+}
+
+module.exports = Task;
